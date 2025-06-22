8629924e9f452d65f2cea23a10e734ad7a6e58ba
PetrusAriaa <petrusariacr25@gmail.com>
2025-06-22 18:14:36 +0700

    Setup installation for cpanel

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load()
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router.Run("127.0.0.1:53270")
 }
