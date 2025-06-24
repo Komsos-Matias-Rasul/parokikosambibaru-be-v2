6473bc8ba576ecb1284138ded1520cdfed077292
PetrusAriaa <petrusariacr25@gmail.com>
2025-06-24 13:24:39 +0700

    fix: add cors middleware

--- main.go
+++ main.go
@@ -9,8 +9,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func main() {
 	router := gin.Default()
+	router.Use(corsMiddleware())
 	db := lib.GetDB()
 	defer db.Close()
 
