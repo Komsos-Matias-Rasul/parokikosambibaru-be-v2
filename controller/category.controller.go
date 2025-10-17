d4c7e81172fb5d3f10521036da25859a8acce79d
PetrusAriaa <petrusariacr25@gmail.com>
2025-10-17 13:24:06 +0700

    refactor: add logging to category controller

--- controller/category.controller.go
+++ controller/category.controller.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"log"
-	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -12,7 +10,7 @@ func (c *Controller) GetCategoriesByEdition(ctx *gin.Context) {
 	editionIdParam := ctx.Query("edition")
 	editionId, err := strconv.Atoi(editionIdParam)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid edition id"})
+		c.res.AbortInvalidEdition(ctx, err, err.Error(), nil)
 		return
 	}
 
@@ -25,7 +23,7 @@ func (c *Controller) GetCategoriesByEdition(ctx *gin.Context) {
 	`, editionId)
 
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.res.AbortDatabaseError(ctx, err, nil)
 		return
 	}
 	defer rows.Close()
@@ -39,30 +37,28 @@ func (c *Controller) GetCategoriesByEdition(ctx *gin.Context) {
 	for rows.Next() {
 		var cat Category
 		if err := rows.Scan(&cat.ID, &cat.Label); err != nil {
-			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.res.AbortDatabaseError(ctx, err, nil)
 			return
 		}
 		categories = append(categories, cat)
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"data": categories})
+	c.res.SuccessWithStatusOKJSON(ctx, nil, categories)
 }
 
 func (c *Controller) GetCategoriesByArticle(ctx *gin.Context) {
 	articleIdParam := ctx.Query("article")
 	articleId, err := strconv.Atoi(articleIdParam)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid article id"})
+		c.res.AbortInvalidArticle(ctx, err, err.Error(), nil)
 		return
 	}
 
 	rows, err := c.db.Query(
 		"SELECT id, label, `key` FROM categories WHERE edition_id = (SELECT edition_id FROM articles WHERE articles.id = ?) OR categories.id = 1 ORDER BY id",
 		articleId)
-
 	if err != nil {
-		log.Println(err)
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.res.AbortDatabaseError(ctx, err, nil)
 		return
 	}
 	defer rows.Close()
@@ -77,12 +73,11 @@ func (c *Controller) GetCategoriesByArticle(ctx *gin.Context) {
 	for rows.Next() {
 		var cat Category
 		if err := rows.Scan(&cat.ID, &cat.Label, &cat.Key); err != nil {
-			log.Println(err)
-			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.res.AbortDatabaseError(ctx, err, nil)
 			return
 		}
 		categories = append(categories, cat)
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"data": categories})
+	c.res.SuccessWithStatusOKJSON(ctx, nil, categories)
 }
